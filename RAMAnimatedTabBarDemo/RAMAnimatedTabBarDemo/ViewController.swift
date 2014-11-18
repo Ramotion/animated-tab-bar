f54f3596cd3bc0ac1911ce693ec39e392a15c6c6
Alex Kalinkin <alex.k@ramotion.com>
2014-11-18 17:47:05 +0300

    added project

---
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RAMAnimatedTabBarDemo
+//
+//  Created by Alex Kalinkin on 11/18/14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemoTests/RAMAnimatedTabBarDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  RAMAnimatedTabBarDemoTests.swift
+//  RAMAnimatedTabBarDemoTests
+//
+//  Created by Alex Kalinkin on 11/18/14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RAMAnimatedTabBarDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
