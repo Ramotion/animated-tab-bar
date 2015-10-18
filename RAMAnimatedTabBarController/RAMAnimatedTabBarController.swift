179acd73a26f59e5317b5fa88b8af3142c9c694e
Khoa Pham <onmyway133@gmail.com>
2015-10-18 22:21:49 +0700

    Add setupContainers because viewDidLoad is called at init, at which tabBar.items is not available yet

--- RAMAnimatedTabBarController/RAMAnimatedTabBarController.swift
+++ RAMAnimatedTabBarController/RAMAnimatedTabBarController.swift
@@ -58,6 +58,12 @@ public class RAMAnimatedTabBarController: UITabBarController {
         createCustomIcons(containers)
     }
 
+    public func setupContainers() {
+        let containers = createViewContainers()
+
+        createCustomIcons(containers)
+    }
+
 // MARK: create methods
 
     func createCustomIcons(containers : [String: UIView]) {
