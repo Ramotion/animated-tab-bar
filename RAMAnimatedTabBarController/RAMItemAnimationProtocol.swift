5dce00868de285a746bde739a98de72ae35687f8
Alex.k <Alex.k@ramotion.com>
2015-12-18 15:20:13 +0300

    Changed default textSelectedColor

--- RAMAnimatedTabBarController/RAMItemAnimationProtocol.swift
+++ RAMAnimatedTabBarController/RAMItemAnimationProtocol.swift
@@ -34,7 +34,7 @@ protocol RAMItemAnimationProtocol {
 class RAMItemAnimation: NSObject, RAMItemAnimationProtocol {
 
     @IBInspectable var duration : CGFloat = 0.5
-    @IBInspectable var textSelectedColor: UIColor = UIColor.blackColor()
+    @IBInspectable var textSelectedColor: UIColor = UIColor.init(red: 0, green: 0.478431, blue: 1, alpha: 1)
     @IBInspectable var iconSelectedColor: UIColor!
 
     func playAnimation(icon : UIImageView, textLabel : UILabel) {
