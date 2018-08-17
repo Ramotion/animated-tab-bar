85a4087618012e83cfbce790acf73af43a2e0210
Alex K <kalinkin.aleksey@gmail.com>
2018-08-17 10:03:08 +0300

    Add Unit Tests target, planing write test soon

---
+++ RAMAnimatedTabBarDemo/AnimatedTabBarTests/AnimatedTabBarTests.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedTabBarTests.swift
+//  AnimatedTabBarTests
+//
+//  Created by Alex K on 17/08/2018.
+//  Copyright © 2018 Ramotion. All rights reserved.
+//
+
+import XCTest
+
+class AnimatedTabBarTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+}
