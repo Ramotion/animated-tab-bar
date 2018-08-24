d88a779dbc722c8b6f1ebb713f3834527f271989
Alex K <kalinkin.aleksey@gmail.com>
2018-08-24 10:52:15 +0300

    add first test

--- RAMAnimatedTabBarDemo/AnimatedTabBarTests/AnimatedTabBarTests.swift
+++ RAMAnimatedTabBarDemo/AnimatedTabBarTests/AnimatedTabBarTests.swift
@@ -7,12 +7,23 @@
 //
 
 import XCTest
+@testable import Animated_Tab_Bar
 
 class AnimatedTabBarTests: XCTestCase {
+    
+    var tabBarController: RAMAnimatedTabBarController!
+    
     override func setUp() {
         super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        tabBarController = storyboard.instantiateViewController(withIdentifier: "RAMAnimatedTabBarController") as? RAMAnimatedTabBarController
+        
     }
     override func tearDown() {
         super.tearDown()
+    }
+    
+    func testCreateController() {
+        XCTAssertNotNil(tabBarController)
     }
 }
